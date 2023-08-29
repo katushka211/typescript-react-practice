ebb537a5321b308158843e9f6c83fa601c012547
Kateryna Verveda <katushka211@gmail.com>
2023-08-29 19:12:43 +0200

    add examples

---
+++ dist/module2.js
@@ -0,0 +1,62 @@
+// boolean
+let isDone = false;
+// number
+let decimal = 6;
+let float = 3.14;
+let hex = 0xf00d;
+let binary = 0b1010;
+let octal = 0o744;
+// string
+let color = "yellow";
+// null
+let empty = null;
+//undefined
+let notParam = undefined;
+function foo(num, str, bool, empty) {
+    //Some logic
+}
+function foo1(num = 10, str = "Some string", bool = true, empty = null) {
+    // Some logic
+}
+// object
+const obj = {};
+const obj1 = {};
+let user;
+let user1 = {
+    name: "Kate",
+    age: 36,
+};
+let userRoman = {
+    name: "Roman",
+    age: 3,
+};
+let owner = {
+    name: "Bob",
+    age: 40,
+};
+// array
+let arrString;
+arrString = ["5", "hello", "6", "7"];
+let arrNumber;
+arrNumber = [1, 10, 58, 74];
+let matrix = [
+    [1, 2],
+    [3, 4],
+];
+let mixed = [1, "two"];
+// generic
+let numbers = [1, 2, 3, 4, 5];
+let users = [
+    { name: "Tom", age: 30 },
+    { name: "Jack", age: 25 },
+    { name: "Alice", age: 32 },
+];
+let books = [
+    { title: "Ocean", quantity: 5 },
+    { title: "Lucky", quantity: 4 },
+    { title: "Secret", quantity: 1 },
+];
+//any
+let arrAny;
+arrAny = [4, "Secret", "one", { title: "second", number: 1 }, [1, 4, 7]];
+//# sourceMappingURL=module2.js.map
