849e46de04ff96332761d28eb02a876c8e1df21d
Kateryna Verveda <katushka211@gmail.com>
2023-09-01 09:26:04 +0200

    update module.02

--- src/module2.ts
+++ src/module2.ts
@@ -119,3 +119,125 @@ date = [7, 11, 2023]; // OK
 
 let tuple: [string, ...number[]];
 tuple = ["hello", 42, 100, 200]; // OK
+
+//enum
+enum Role {
+  ADMIN,
+  USER,
+}
+const person = {
+  role: Role.ADMIN,
+};
+if (person.role === Role.ADMIN) {
+  console.log("Role: ", Role.ADMIN);
+}
+
+//const enum
+const enum HttpCodes {
+  OK = 200,
+  BadRequest = 400,
+  Unauthorized = 401,
+}
+const status1 = HttpCodes.OK;
+
+//Union Type
+let mixedType: string | number | boolean;
+mixedType = "string";
+mixedType = 10;
+mixedType = true;
+// mixedType = {}; //error
+
+function combine(param1: number | string, param2: number | string) {
+  if (typeof param1 === "number" && typeof param2 === "number") {
+    return param1 + param2;
+  } else {
+    return param1.toString() + param2.toString();
+  }
+}
+
+//union type з об'єктами
+type Dog = {
+  legs: 4;
+  bark: () => void;
+};
+
+type Fish = {
+  fins: 2;
+  swim: () => void;
+};
+
+let pet: Dog | Fish;
+
+// type guard function
+function isDog(pet: Dog | Fish): pet is Dog {
+  return "bark" in pet;
+}
+
+pet = { legs: 4, bark: () => {} };
+
+// Перевіряємо, чи є наш вихованець собакою перед тим, як використовувати метод bark
+if (isDog(pet)) {
+  pet.bark();
+} else {
+  pet = {
+    fins: 2,
+    swim: () => {
+      // реалізація методу
+    },
+  };
+  pet.swim(); // TypeScript знає, що якщо pet не Dog, то це має бути Fish
+}
+
+//Intersection Type (символ &)
+type Employee = {
+  name: string;
+  id: number;
+};
+
+type Manager = {
+  employees: Employee[];
+};
+
+type CEO = Employee & Manager;
+
+const ceo: CEO = {
+  name: "Alice",
+  id: 1,
+  employees: [
+    {
+      name: "Bob",
+      id: 2,
+    },
+  ],
+};
+
+//Literal Type
+type OneOrTwo = 1 | 2;
+let value: OneOrTwo;
+value = 1;
+value = 2;
+// value = 3; // Error: Type '3' is not assignable to type 'OneOrTwo'.
+
+type YesOrNo = "yes" | "no";
+let answer: YesOrNo;
+answer = "yes";
+answer = "no";
+// answer = 'maybe'; // Error: Type '"maybe"' is not assignable to type 'YesOrNo'.
+
+type ButtonSize = "small" | "medium" | "large";
+
+function getButtonStyle(size: ButtonSize) {
+  switch (size) {
+    case "small":
+      return { fontSize: "10px", padding: "5px" };
+    case "medium":
+      return { fontSize: "14px", padding: "10px" };
+    case "large":
+      return { fontSize: "18px", padding: "15px" };
+    default:
+      return { fontSize: "14px", padding: "10px" };
+  }
+}
+
+let myButtonStyle = getButtonStyle("medium"); // OK
+// myButtonStyle = getButtonStyle('extra-large'); // Error: Argument of type '"extra-large"' is not assignable to parameter of type 'ButtonSize'.
