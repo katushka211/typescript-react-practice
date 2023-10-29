014fca6f4e43c675c24eec9e7e62307f05b3fab6
Kateryna Verveda <katushka211@gmail.com>
2023-10-29 18:50:42 +0100

    add Generic Classes

--- src/module2generic.ts
+++ src/module2generic.ts
@@ -123,4 +123,50 @@ function extractValue<T extends object, U extends keyof T>(obj: T, key: U) {
 }
 
 extractValue({ name: "John" }, "name");
-//////////////
+
+//////////////   Generic Classes   ////////
+
+class DataStorage<T> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Hello");
+textStorage.addItem("World");
+console.log(textStorage.getItems()); // ['Hello', 'World']
+// textStorage.addItem(1); // Error
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(1);
+numberStorage.addItem(2);
+console.log(numberStorage.getItems()); // [1, 2]
+// numberStorage.addItem('TEXT'); // Error
+
+/////////////
+class KeyValuePair<TKey, TValue> {
+  constructor(private key: TKey, private value: TValue) {}
+
+  getKey(): TKey {
+    return this.key;
+  }
+
+  getValue(): TValue {
+    return this.value;
+  }
+}
+
+const pair1 = new KeyValuePair("name", "Alice");
+console.log(pair1.getKey()); // 'name'
+console.log(pair1.getValue()); // 'Alice'
+
+const pair2 = new KeyValuePair(1, true);
+console.log(pair2.getKey()); // 1
+console.log(pair2.getValue()); // true
