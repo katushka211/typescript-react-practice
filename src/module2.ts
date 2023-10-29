ac134668ff12e0db5a3abda3b0e9fdf9950afa47
Kateryna Verveda <katushka211@gmail.com>
2023-10-29 16:35:31 +0100

    add instanceof

--- src/module2.ts
+++ src/module2.ts
@@ -460,4 +460,79 @@ class OtherLeon implements Leon {
   bark: "Wooo";
 }
 
-////
+////Advanced Types
+//Type Guards
+
+//typeof
+
+type ComplexType = string | number;
+
+// function combine1(a: ComplexType, b: ComplexType) {
+//   return a + b;
+// }  Тут ми отримаємо помилку, оскільки TypeScript не знає, як працювати з цими типами.
+function combine2(a: ComplexType, b: ComplexType) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+////    in
+type Admin1 = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee1 = {
+  name: string;
+  startDate: Date;
+};
+
+type AdminOrEmployee = Admin1 | Employee1;
+
+function printDetails(obj: AdminOrEmployee) {
+  console.log(`Name: ${obj.name}`);
+
+  if ("privileges" in obj) {
+    console.log(`Privileges: ${obj.privileges.join(", ")}`);
+  }
+
+  if ("startDate" in obj) {
+    console.log(`Start Date: ${obj.startDate}`);
+  }
+}
+
+///////////////////////instanceof/////////////////////
+class Car {
+  drive() {
+    console.log("Driving a car...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+
+  loadCargo(amount: number) {
+    console.log(`Loading cargo: ${amount}`);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const carInstance = new Car();
+const truckInstance = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(carInstance);
+useVehicle(truckInstance);
+
+///////////////
