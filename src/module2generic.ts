341f75b4a1b1d52520e39c8712bd48b3f1df9ab2
Kateryna Verveda <katushka211@gmail.com>
2023-10-29 18:40:36 +0100

    add Keyof

--- src/module2generic.ts
+++ src/module2generic.ts
@@ -85,3 +85,37 @@ function arrayLogger<T extends Array<string>>(array: T): void {
 
 arrayLogger(["Hello", "World"]); // Ok
 // arrayLogger([1, 2, 3]); // Error
+
+/////////////////     keyof       ////////////////
+
+type Actor = {
+  name: string;
+  age: number;
+  location: string;
+};
+type ActorKeys = keyof Actor; // 'name' | 'age' | 'location'
+
+//////
+type Person = {
+  name: string;
+  age: number;
+  location: string;
+};
+
+type PersonKeys = keyof Person; // 'name' | 'age' | 'location'
+
+function getPersonInfo(person: Person, key: PersonKeys) {
+  return person[key];
+}
+
+const john: Person = {
+  name: "John",
+  age: 25,
+  location: "NY",
+};
+
+console.log(getPersonInfo(john, "age")); // 25
+console.log(getPersonInfo(john, "name")); // 'John'
+// console.log(getPersonInfo(john, "job")); // Error: Argument of type '"job"' is not assignable to parameter of type 'PersonKeys'.
+
+export {};
